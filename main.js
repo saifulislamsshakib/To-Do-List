403d15851e20159e69fdab87f834e81c5222ad97
Saiful Islam <saifulislamsshakib@gmail.com>
2025-07-04 18:41:03 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const searchBox = document.getElementsByClassName("search-Box")[0];
+const listContiner = document.getElementById("list-container");
+
+function addTask(){
+    if(searchBox.value ===''){
+        alert("Write something");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = searchBox.value;
+        listContiner.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML= "\u00d7";
+        li.appendChild(span);
+    }
+    searchBox.value = "";
+    saveData();
+
+}
+
+listContiner.addEventListener("click", function(e){
+   if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+   else if(e.target.tagName === "SPAN") {
+              e.target.parentElement.remove();
+              saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContiner.innerHTML)
+}
+function showData(){
+    listContiner.innerHTML = localStorage.getItem("data");
+}
+showData()
